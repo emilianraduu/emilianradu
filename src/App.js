d3c5ae839ca60154222f1207a375bf074c26cd3b
Emilian Radu <emiradu98@icloud.com>
2022-08-18 17:41:49 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
-import {Main} from "./components/Main";
 
 function App() {
   return (
     <div className="App">
-            {/* <Main/> */}
     </div>
   );
 }
