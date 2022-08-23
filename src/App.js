6d4a787db10afe42cb7185553341d1d5e0ed5b60
Emilian Radu <emiradu98@icloud.com>
2022-08-23 18:34:08 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
-
+import Lottie from 'react-lottie';
+import * as animationData from './assets/working.json'
+ 
 function App() {
   return (
     <div className="App">
-      <div>salut</div>
+          <Lottie
+              height={400}
+              width={400}
+             loop={true}/>
     </div>
   );
 }
