3236502f79398997dc169e98af0bce71eea533fb
Emilian Radu <emiradu98@icloud.com>
2022-08-23 18:36:22 +0300

    test

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import './App.css';
 import Lottie from 'react-lottie';
 import * as animationData from './assets/working.json'
- 
+
 function App() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
   return (
     <div className="App">
-          <Lottie
-              height={400}
-              width={400}
-             loop={true}/>
+      <Lottie options={defaultOptions}
+        height={400}
+        width={400}
+        loop={true} />
     </div>
   );
 }
