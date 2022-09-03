00939154bc91769a3ef96359933cc5104e392ed1
Emilian Radu <emiradu98@icloud.com>
2022-09-03 22:27:05 +0300

    deploy fix

--- src/components/fileTypes/OutsideLink.js
+++ src/components/fileTypes/OutsideLink.js
@@ -3,7 +3,7 @@ import './style.scss'
 
 const OutsideLink = ({pathname = '/', text, image = require('../../assets/icons/github.png')}) => {
     return (
-        <a href={pathname} target={"_blank"} className={'file'}>
+        <a href={pathname} rel="noopener noreferrer" target={"_blank"} className={'file'}>
             <img alt={'folder'} src={image} className={'icon'} />
             <p>{text}</p>
         </a>
